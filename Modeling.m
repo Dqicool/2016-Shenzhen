0b6fb003e09146004d730004a3b51ecbf2d637b7
Dqicool <dqicool@outlook.com>
2017-07-27 15:00:55 +0800

    undone

--- Modeling.m
+++ Modeling.m
@@ -33,28 +33,47 @@ function [InitialDataAmong, InitialDelay, DownloadTempPool] = ModelI(E2ERTT, Ini
 end
 
 function [PauseTotal, PauseCount] = ModelP(DownloadTempPool, PlayAvgSpeed, CodeSpeed, E2ERTT, RndCS, Replay)
-    global DataSize
-    time                = zeros(DataSize, 1);
-    PauseTotal          = zeros(DataSize, 1);
-    StartSymbol         = true (DataSize, 1);
-    PauseCount          = zeros(DataSize, 1);
-    SendT               = 4288 ./ PlayAvgSpeed;                                                    %引入发送时间概念
-    FinishSymbol        = false(DataSize, 1);                                                      %播放结束标志
-    PkgPRTT             = E2ERTT ./ SendT;
-    PkgSend             = ones (DataSize, 1);
-    DownloadTempPool    = 4128 .* PkgPRTT;
-    while sum(FinishSymbol) < DataSize
-        time                = time + SendT;
-        PkgSend             = PkgSend + 1;
-        FinishSymbol        = (time(end) >= 30000);
-        PlayTime            = time - PauseTotal;                                                    %播放时间
-        Addins              = 4128 .* (mod(PkgSend, 1e4) > PkgPRTT);                                %4288 - 160
-        PlayedOut           = 1.1 .* StartSymbol .* CodeSpeed .* RndCS(1 + fix(PlayTime)) .* SendT;
-        DownloadTempPool    = DownloadTempPool - PlayedOut + Addins;
-        PauseJudge          = DownloadTempPool < CodeSpeed .* RndCS(1 + fix(PlayTime)) .* SendT;
-        PauseCount          = PauseCount +  PauseJudge .* StartSymbol;                              %卡段时间
-        StartSymbol         = StartSymbol - PauseJudge .* StartSymbol + ...                         %刚刚开始卡顿的数目
-                              (~StartSymbol) .* (DownloadTempPool > Replay .* CodeSpeed);             %卡顿还没有开始的数目
-        PauseTotal          = PauseTotal + (~StartSymbol) .* SendT;
+    StartSymbol = true;
+    RTTs = E2ERTT;
+    RTTd = 0;
+    time = 0;
+    pkg  = 1;
+    Pipe = struct('PkgNo',1,'SendTimeStamp',0,'RecTimePre',E2ERTT,'Acked',0);
+    Cwnd = PlayAvgSpeed .* E2ERTT ./ 4288;
+    SndT = E2ERTT ./ Cwnd;
+    while time < 30000
+        %一般性新
+        pkg  = pkg + 1;
+        time = time + SndT;
+        RTTc = E2ERTT .* MaxwellRnd(1);
+        %新流体点
+        Pipe.PkgNo(end + 1) = pkg;
+        Pipe.SendTimeStamp(end + 1) = time;
+        Pipe.RecTimePre(end + 1) = RTTc;
+        Pipe.Acked(end + 1) = 0;
+        %新RTO
+        RTTs = 0.875 .* RTTs + 0.125 .* RTTc;
+        RTTd = 0.75 .* RTTd + 0.25 .* abs(RTTs - RTTc);
+        RTO  = RTTs + 4 .* RTTd;
+        %考察已接收的包
+        Pipe.Acked = (time - Pipe.SendTimeStamp) > Pipe.RecTimePre;
+        PkgAddin = find(Pipe.Acked == 0, 1, 'first') - 1; 
+        %从流中去掉已经顺序收到的包
+        Pipe.PkgNo = Pipe.PkgNo((PkgAddin + 1):end);
+        Pipe.SendTimeStamp = Pipe.SendTimeStamp((PkgAddin + 1):end);
+        Pipe.RecTimePre = Pipe.RecTimePre((PkgAddin + 1):end);
+        Pipe.Acked = Pipe.Acked((PkgAddin + 1):end);
+        %考察超时重传和快恢复并对流体进行清零
+        if ((time - Pipe.SendTimeStamp(1)) > RTO) || (sum(Pipe.Acked) >= 2)
+            Pipe = struct('PkgNo',Pipe.PkgNo(1),'SendTimeStamp',time,'RecTimePre',E2ERTT,'Acked',0);
+            pkg  = Pipe.PkgNo(1);
+        end
+        %池子变化
+        if StartSymbol == true
+            DownloadTempPool = DownloadTempPool + PkgAddin .* 4128 - CodeSpeed .* RndCS();
+        else
+            DownloadTempPool = DownloadTempPool + PkgAddin .* 4128;
+        end
+
     end
 end
